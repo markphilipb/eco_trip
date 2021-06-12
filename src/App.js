f8106fa0d6b5954792719c61cc25c80d1430415d
Mark <mark.balazon@gmail.com>
2021-06-12 13:01:40 -0400

    resolved package.json and eslint dependency issue

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./components/Home/Home";
 import Map from "./components/MapBoxComponent/MapBoxComponent";
-import HomeGoogle from "./HomeGoogle/HomeGoogle";
 
 function App() {
   return (
     <div className="App">
       <Home />
-      {/* <HomeGoogle /> */}
-      {/* <GoogleMapTwo /> */}
     </div>
   );
 }
