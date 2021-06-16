9d9d6bb4637133a6b01fc0cbec86bf9c1c812fc0
Mark <mark.balazon@gmail.com>
2021-06-15 23:56:47 -0400

    added navbar to home componenet

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,9 @@
 import logo from "../../imgs/eco_logo.png";
 
 export default function NavBar(props) {
-  return <div></div>;
+  return (
+    <div>
+      <img src={logo} alt="logo" />
+    </div>
+  );
 }
