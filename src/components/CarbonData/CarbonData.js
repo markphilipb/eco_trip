e0db55ab5d42ec404e3db09c67233368e7c4aba0
Mark <mark.balazon@gmail.com>
2021-06-16 23:16:37 -0400

    adding flexbox support to carbon data component

--- src/components/CarbonData/CarbonData.js
+++ src/components/CarbonData/CarbonData.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Container from "react-bootstrap/esm/Container";
 
 class CarbonData extends Component {
   constructor(props) {
@@ -12,12 +13,14 @@ class CarbonData extends Component {
   render() {
     return (
       <div>
-        <div>Distance: {this.props.distance} meters</div>
-        <div>Carbon emission is {0.25 * this.props.distance} grams</div>
-        <div>
-          Your trip costs ${((this.props.distance / 34761.81) * 3).toFixed(2)}{" "}
-          USD
-        </div>
+        <Container fluid>
+          <div>Distance: {this.props.distance} meters</div>
+          <div>Carbon emission is {0.25 * this.props.distance} grams</div>
+          <div>
+            Your trip costs ${((this.props.distance / 34761.81) * 3).toFixed(2)}{" "}
+            USD
+          </div>
+        </Container>
       </div>
     );
   }
