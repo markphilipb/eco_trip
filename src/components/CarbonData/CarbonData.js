085ada612e9e92a4c8e7ca279fb2e242b38b26ec
Mark <mark.balazon@gmail.com>
2021-06-17 20:16:48 -0400

    added suv carbon info

--- src/components/CarbonData/CarbonData.js
+++ src/components/CarbonData/CarbonData.js
@@ -15,9 +15,17 @@ class CarbonData extends Component {
       <div>
         <Container fluid>
           <div>Distance: {this.props.distance} meters</div>
-          <div>Carbon emission is {0.25 * this.props.distance} grams</div>
+          <div>
+            Average passenger car: Carbon emission is{" "}
+            {0.25 * this.props.distance} grams
+          </div>
           <div>
             Your trip costs ${((this.props.distance / 34761.81) * 3).toFixed(2)}{" "}
+            USD
+          </div>
+          <div>SUV: Carbon emission is {this.props.distance} grams</div>
+          <div>
+            Your trip costs ${((this.props.distance / 64373.8) * 3).toFixed(2)}{" "}
             USD
           </div>
         </Container>
