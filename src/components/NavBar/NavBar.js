e3cf573f831361fff03937924a726fed0ee0ca78
Mark <mark.balazon@gmail.com>
2021-06-19 22:54:40 -0400

    changed logo size

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,7 +3,7 @@ import logo from "../../imgs/logo/eco_logo.png";
 export default function NavBar(props) {
   return (
     <div>
-      <img src={logo} alt="logo" height="200" width="200" />
+      <img src={logo} alt="logo" height="150" width="150" />
     </div>
   );
 }
