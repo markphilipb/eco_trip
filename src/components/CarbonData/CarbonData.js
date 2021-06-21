e33524cff1acdd93be41ab146df417730b95cdf1
Mark <mark.balazon@gmail.com>
2021-06-20 20:39:34 -0400

    removed errors on this.props since it is not a class componenet anymore

--- src/components/CarbonData/CarbonData.js
+++ src/components/CarbonData/CarbonData.js
@@ -16,29 +16,24 @@ export default function CarbonData(props) {
     <div>
       {/* flexbox */}
       <Container fluid>
-        <div>Distance: {this.props.distance} meters</div>
+        <div>Distance: {props.distance} meters</div>
         {/* passenger car */}
         <div>
-          Average passenger car: Carbon emission is {0.25 * this.props.distance}{" "}
+          Average passenger car: Carbon emission is {0.25 * props.distance}{" "}
           grams
         </div>
         <div>
-          Your trip costs ${((this.props.distance / 34761.81) * 3).toFixed(2)}{" "}
-          USD
+          Your trip costs ${((props.distance / 34761.81) * 3).toFixed(2)} USD
         </div>
         {/* suv */}
-        <div>SUV: Carbon emission is {0.27 * this.props.distance} grams</div>
+        <div>SUV: Carbon emission is {0.27 * props.distance} grams</div>
         <div>
-          Your trip costs ${((this.props.distance / 32186.9) * 3).toFixed(2)}{" "}
-          USD
+          Your trip costs ${((props.distance / 32186.9) * 3).toFixed(2)} USD
         </div>
         {/* pickup truck */}
+        <div>Pickup truck: Carbon emission is {0.3 * props.distance} grams</div>
         <div>
-          Pickup truck: Carbon emission is {0.3 * this.props.distance} grams
-        </div>
-        <div>
-          Your trip costs ${((this.props.distance / 28968.2) * 3).toFixed(2)}{" "}
-          USD
+          Your trip costs ${((props.distance / 28968.2) * 3).toFixed(2)} USD
         </div>
       </Container>
     </div>
