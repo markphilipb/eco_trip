72f3e51ff71de49166b08eba632e871b6544784b
Mark <mark.balazon@gmail.com>
2021-06-22 21:25:17 -0400

    fixed alignment, aligned to left

--- src/components/CarbonData/CarbonData.js
+++ src/components/CarbonData/CarbonData.js
@@ -54,7 +54,7 @@ export default function CarbonData(props) {
       <Container fluid>
         <div>Distance: {props.distance} meters</div>
 
-        <Row className="w-100 justify-content-center mt-5">
+        <Row className="w-100 justify-content-left mt-2">
           <Col md="auto">
             <img src={carIcon} height="50px" width="50px" alt="carIcon" />
           </Col>
@@ -67,7 +67,7 @@ export default function CarbonData(props) {
           </div>
         </Row>
 
-        <Row className="w-100 justify-content-center mt-2">
+        <Row className="w-100 justify-content-left mt-2">
           <Col md="auto">
             <img src={suvIcon} height="50px" width="50px" alt="suv" />
           </Col>
@@ -80,7 +80,7 @@ export default function CarbonData(props) {
           </div>
         </Row>
 
-        <Row className="w-100 justify-content-center mt-2">
+        <Row className="w-100 justify-content-left mt-2">
           <Col md="auto">
             <img src={pickupIcon} height="50px" width="50px" alt="pickupIcon" />
           </Col>
