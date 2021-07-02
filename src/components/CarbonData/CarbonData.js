9017a4d5632ea8c71a8450056300d8ef0b9374e3
Mark <mark.balazon@gmail.com>
2021-07-01 20:21:11 -0400

    added co2 emission inside spring meter

--- src/components/CarbonData/CarbonData.js
+++ src/components/CarbonData/CarbonData.js
@@ -54,10 +54,15 @@ export default function CarbonData(props) {
             <img src={carIcon} height="50px" width="50px" alt="carIcon" />
           </Col>
           <Col md="auto">
-            <SpringMeter open={open} width={carDist} color="#32DF89" />
+            <SpringMeter
+              open={open}
+              width={carDist}
+              carbonEmit={0.25 * props.distance}
+              color="#32DF89"
+            />
             {/* <div>Carbon emission is {0.25 * props.distance} grams</div> */}
           </Col>
-          <div className="mt-2">
+          <div className="price">
             ${((props.distance / 34761.81) * 3).toFixed(2)} USD
           </div>
         </Row>
@@ -67,10 +72,17 @@ export default function CarbonData(props) {
             <img src={suvIcon} height="50px" width="50px" alt="suv" />
           </Col>
           <Col md="auto">
-            <SpringMeter open={open} width={suvDist} color="#32DF89" />
+            <SpringMeter
+              open={open}
+              width={suvDist}
+              carbonEmit={0.27 * props.distance}
+              color="#32DF89"
+            />
             {/* <div>Carbon emission is {0.27 * props.distance} grams</div> */}
           </Col>
-          <div>${((props.distance / 32186.9) * 3).toFixed(2)} USD</div>
+          <div className="price">
+            ${((props.distance / 32186.9) * 3).toFixed(2)} USD
+          </div>
         </Row>
 
         <Row className="w-100 justify-content-left mt-2">
@@ -78,10 +90,17 @@ export default function CarbonData(props) {
             <img src={pickupIcon} height="50px" width="50px" alt="pickupIcon" />
           </Col>
           <Col md="auto">
-            <SpringMeter open={open} width={truckDist} color="#32DF89" />
+            <SpringMeter
+              open={open}
+              width={truckDist}
+              carbonEmit={0.3 * props.distance}
+              color="#32DF89"
+            />
             {/* <div>Carbon emission is {0.3 * props.distance} grams</div> */}
           </Col>
-          <div>${((props.distance / 28968.2) * 3).toFixed(2)} USD</div>
+          <div className="price">
+            ${((props.distance / 28968.2) * 3).toFixed(2)} USD
+          </div>
         </Row>
       </Container>
     </div>
